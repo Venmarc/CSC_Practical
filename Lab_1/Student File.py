20dba3798be7d54b717cb37c8320fccfcda2bffc
Venmarc <venmarc@protonmail.com>
2025-12-19 11:26:36 +0100

    Remove `reg_no` variable and its corresponding print statement.

--- Lab_1/Student File.py
+++ Lab_1/Student File.py
@@ -1,8 +1,6 @@
 welcome_message = "Welcome to my program!\nI'm Mario"
 name = "Eddie-Nkwoh Chijioke Victor-Mario"
-reg_no = "MOUAU/CMP/24/123560"
+department = "Computer Science"
 print(welcome_message)
 print("Name: ", name)
-print("Reg.No.: ", reg_no)
-department = "Computer Science"
 print("Department:", department)
